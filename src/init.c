6507c397243209a45e29bac843c4a9febdb25add
Yelie-Yuan <yelie.yuan@uconn.edu>
2021-02-03 16:24:59 +0800

    rewrite rpanet_cpp

--- src/init.c
+++ src/init.c
@@ -13,7 +13,7 @@ extern void netSim(void *, void *, void *, void *, void *, void *, void *, void 
 /* .Call calls */
 extern SEXP _wdnet_fx(SEXP, SEXP, SEXP);
 extern SEXP _wdnet_hello_world();
-extern SEXP _wdnet_rpanet_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _wdnet_rpanet_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
     {"netSim", (DL_FUNC) &netSim, 12},
@@ -21,9 +21,9 @@ static const R_CMethodDef CEntries[] = {
 };
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_wdnet_fx",          (DL_FUNC) &_wdnet_fx,          3},
-    {"_wdnet_hello_world", (DL_FUNC) &_wdnet_hello_world, 0},
-    {"_wdnet_rpanet_cpp",  (DL_FUNC) &_wdnet_rpanet_cpp,  9},
+    {"_wdnet_fx",          (DL_FUNC) &_wdnet_fx,           3},
+    {"_wdnet_hello_world", (DL_FUNC) &_wdnet_hello_world,  0},
+    {"_wdnet_rpanet_cpp",  (DL_FUNC) &_wdnet_rpanet_cpp,  12},
     {NULL, NULL, 0}
 };
 
@@ -32,4 +32,3 @@ void R_init_wdnet(DllInfo *dll)
     R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
-
--- src/rpanet.cpp
+++ src/rpanet.cpp
@@ -1,53 +1,70 @@
-#include <RcppArmadilloExtensions/sample.h>
-
+#include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
+int my_sample(int tnode, double s_strength, arma::vec strength) {
+  int i;
+  double j = 0, rT;
+  rT = unif_rand() * s_strength;
+  for (i = 0; i < tnode; i++) {
+    j += strength[i];
+    if (j >= rT)
+      break;
+  }
+  return i + 1;
+}
 
 // [[Rcpp::export]]
 Rcpp::List rpanet_cpp(int       nsteps,
-	                   arma::vec  control,
-	                   const bool directed, 
-	                   arma::vec  m,
-	                   arma::vec  w,
-	                   arma::vec  outstrength,
-	                   arma::vec  instrength, 
-	                   int        nnode,
-	                   int        tnode) {
+                      arma::vec  control,
+                      const bool directed, 
+                      arma::vec  m,
+                      arma::vec  w,
+                      arma::vec  u,
+                      arma::vec  outstrength,
+                      arma::vec  instrength, 
+                      double     s_outstrength,
+                      double     s_instrength,
+                      int        nnode,
+                      int        tnode) {
   double alpha = control[0], beta = control[1], gamma = control[2], xi = control[3];
-  double u;
+  double delta_out = control[4], delta_in = control[5];
+  double u2;
   int v1, v2, count = 0;
-  arma::vec nodes = arma::linspace<arma::vec>(1, outstrength.size(), outstrength.size());
   arma::vec startnode(outstrength.size(), arma::fill::zeros); 
   arma::vec endnode(outstrength.size(), arma::fill::zeros);
-  arma::vec subnode, subinstrength, suboutstrength;
-  
-
   for (int i = 0; i < nsteps; i++) {
     for (int j = 0; j < m[i]; j++) {
-      tnode--;
-      subnode = nodes.subvec(0, tnode);
-      subinstrength = instrength.subvec(0, tnode);
-      suboutstrength = outstrength.subvec(0, tnode);
-      u = R::runif(0, 1);
-      if (u < alpha) {
-      	v1 = nnode + 1;
-      	v2 = Rcpp::RcppArmadillo::sample(subnode, 1L, true, subinstrength)[0];
-      	nnode++;
-      } else if (u < alpha + beta) {
-      	v1 = Rcpp::RcppArmadillo::sample(subnode, 1L, true, suboutstrength)[0];
-      	v2 = Rcpp::RcppArmadillo::sample(subnode, 1L, true, subinstrength)[0];
-      } else if (u < alpha + beta + gamma) {
-      	v1 = Rcpp::RcppArmadillo::sample(subnode, 1L, true, suboutstrength)[0];
-      	v2 = nnode + 1;
-      	nnode++;
-      } else if (u < alpha + beta + gamma + xi) {
-      	v1 = nnode + 1;
-      	v2 = nnode + 2;
-      	nnode = nnode + 2;
+      u2 = u[count];
+      if (u2 < alpha) {
+        v1 = nnode + 1;
+        v2 = my_sample(tnode, s_instrength, instrength);
+        s_outstrength = s_outstrength + w[count] + delta_out;
+        s_instrength = s_instrength + w[count] + delta_in;
+        nnode++;
+      } else if (u2 < alpha + beta) {
+        v1 = my_sample(tnode, s_outstrength, outstrength);
+        v2 = my_sample(tnode, s_instrength, instrength);
+        s_outstrength += w[count];
+        s_instrength += w[count];
+      } else if (u2 < alpha + beta + gamma) {
+        v1 = my_sample(tnode, s_outstrength, outstrength);
+        v2 = nnode + 1;
+        s_outstrength = s_outstrength + w[count] + delta_out;
+        s_instrength = s_instrength + w[count] + delta_in;
+        nnode++;
+      } else if (u2 < alpha + beta + gamma + xi) {
+        v1 = nnode + 1;
+        v2 = nnode + 2;
+        s_outstrength = s_outstrength + w[count] + 2 * delta_out;
+        s_instrength = s_instrength + w[count] + 2 * delta_in;
+        nnode = nnode + 2;
       } else {
         v1 = nnode + 1;
         v2 = nnode + 1;
+        s_outstrength = s_outstrength + w[count] + delta_out;
+        s_instrength = s_instrength + w[count] + delta_in;
         nnode = nnode + 1;
       }
+      
       startnode[count] = v1;
       endnode[count] = v2;
       outstrength[v1 - 1] += w[count];
@@ -55,6 +72,8 @@ Rcpp::List rpanet_cpp(int       nsteps,
       if (! directed) {
         outstrength[v2 - 1] += w[count];
         instrength[v1 - 1] += w[count];
+        s_outstrength += w[count];
+        s_instrength += w[count];
       }
       count++;
       tnode = nnode;
@@ -68,6 +87,6 @@ Rcpp::List rpanet_cpp(int       nsteps,
   ret["outstrength"] = outstrength.subvec(0, tnode - 1);
   return ret;
 }
-  
-  
-			       
+
+
+
